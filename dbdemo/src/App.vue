250e82dabc68e5df47ffb35c156bbf3aa99ed26f
Daniel Schwartz <schdan037@myuct.ac.za>
2018-01-17 15:22:54 +0200

    First Commit:
    - Added all the files
    - ignored the db creds
    - ignored all the . files

---
+++ dbdemo/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container" id="app">
+    <h1>{{ title }}</h1>
+
+    <button @click="fetchAll">Connect to database</button>
+
+    <app-table></app-table>
+
+
+  </div>
+</template>
+
+<script>
+  import AppTable from './AppTable.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      'app-table': AppTable
+    },
+    data () {
+      return {
+        title: "Japanese Whisky Demo",
+      }
+    },
+    methods: {
+      fetchAll() {
+        // connect to db here
+      }
+    }
+  }
+
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ dbdemo/src/AppTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <table class="table table-striped table-bordered">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Size</th>
+          <th scope="col">Volume</th>
+          <th scope="col">Sale Price</th>
+          <th scope="col">Price per bottle</th>
+          <th scope="col">Price per ml</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+    data () {
+      return {}
+    }
+
+  }
+
+</script>
